9a3b136a8c2b08aeb38e9e3190b696691bba4b99
Kovacs Erik-Robert <kovaroberik@gmail.com>
2018-10-19 10:32:19 +0300

    took some elements out in separate components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Launcher } from "react-chat-window";
+import Masthead from "./components/Masthead";
+import Features from "./components/Features";
 import "./theme/vendor/bootstrap/css/bootstrap.min.css";
 import "./theme/vendor/fontawesome-free/css/all.min.css";
 import "./theme/vendor/simple-line-icons/css/simple-line-icons.css";
@@ -29,7 +31,7 @@ class App extends Component {
       .catch(err => console.error(err));
   };
 
-  _sendMessage(text) {
+  _sendMessage = text => {
     if (text.length > 0) {
       this.setState({
         messageList: [
@@ -42,7 +44,7 @@ class App extends Component {
         ]
       });
     }
-  }
+  };
 
   render() {
     return (
@@ -58,84 +60,9 @@ class App extends Component {
           </div>
         </nav>
 
-        <header className="masthead text-white text-center">
-          <div className="overlay" />
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-9 mx-auto">
-                <h1 className="mb-5">Where will you go next?</h1>
-              </div>
-              <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
-                <form onSubmit={e => e.preventDefault()}>
-                  <div className="form-row">
-                    <div className="col-12 col-md-9 mb-2 mb-md-0">
-                      <input
-                        type="email"
-                        className="form-control form-control-lg"
-                        placeholder="Enter your destination..."
-                      />
-                    </div>
-                    <div className="col-12 col-md-3">
-                      <button
-                        type="submit"
-                        className="btn btn-block btn-lg btn-primary"
-                      >
-                        Ask Ionut
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </header>
+        <Masthead />
 
-        <section className="features-icons bg-light text-center">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-4">
-                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                  <div className="features-icons-icon d-flex">
-                    <i className="icon-screen-desktop m-auto text-primary" />
-                  </div>
-                  <h3>Book online</h3>
-                  <p className="lead mb-0">
-                    <strong>Your next vacation</strong> is your fully integrated
-                    solution for travel - from the desktop or mobile, we are
-                    eager to help you plan and book
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                  <div className="features-icons-icon d-flex">
-                    <i className="icon-layers m-auto text-primary" />
-                  </div>
-                  <h3>Next-gen assistance</h3>
-                  <p className="lead mb-0">
-                    Let us introduce you to your Machine Learning-enabled online
-                    assistant, Ionut! Just chat with him and he will recommend
-                    you the best location for
-                    <strong> Your next vacation</strong>!
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                  <div className="features-icons-icon d-flex">
-                    <i className="icon-check m-auto text-primary" />
-                  </div>
-                  <h3>Easy to Use</h3>
-                  <p className="lead mb-0">
-                    You need not worry about weather, traffic or availability -
-                    let Ionut figure it all out and assure you have the best
-                    trip possible.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Features />
 
         <footer className="footer bg-light">
           <div className="container">
---
+++ src/components/Masthead.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Masthead extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messageList: []
+    };
+  }
+
+  render() {
+    return (
+      <header className="masthead text-white text-center">
+        <div className="overlay" />
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-9 mx-auto">
+              <h1 className="mb-5">Where will you go next?</h1>
+            </div>
+            <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+              <form onSubmit={e => e.preventDefault()}>
+                <div className="form-row">
+                  <div className="col-12 col-md-9 mb-2 mb-md-0">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="Enter your destination..."
+                    />
+                  </div>
+                  <div className="col-12 col-md-3">
+                    <button
+                      type="submit"
+                      className="btn btn-block btn-lg btn-primary"
+                    >
+                      Ask Ionut
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Masthead;
