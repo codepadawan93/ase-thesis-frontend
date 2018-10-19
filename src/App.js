e6139f2c6efda3ffc7a72cb7e4c820e15f07d9da
Kovacs <ekovacs@techteam.com>
2018-10-19 18:19:25 +0300

    pulled footer out into func component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Launcher } from "react-chat-window";
 import Masthead from "./components/Masthead";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
+import { Launcher } from "react-chat-window";
 import "./theme/vendor/bootstrap/css/bootstrap.min.css";
 import "./theme/vendor/fontawesome-free/css/all.min.css";
 import "./theme/vendor/simple-line-icons/css/simple-line-icons.css";
@@ -64,61 +65,8 @@ class App extends Component {
 
         <Features />
 
-        <footer className="footer bg-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
-                <ul className="list-inline mb-2">
-                  <li className="list-inline-item">
-                    <a onClick={e => e.preventDefault()} href="/">
-                      About
-                    </a>
-                  </li>
-                  <li className="list-inline-item">&sdot;</li>
-                  <li className="list-inline-item">
-                    <a onClick={e => e.preventDefault()} href="/">
-                      Contact
-                    </a>
-                  </li>
-                  <li className="list-inline-item">&sdot;</li>
-                  <li className="list-inline-item">
-                    <a onClick={e => e.preventDefault()} href="/">
-                      Terms of Use
-                    </a>
-                  </li>
-                  <li className="list-inline-item">&sdot;</li>
-                  <li className="list-inline-item">
-                    <a onClick={e => e.preventDefault()} href="/">
-                      Privacy Policy
-                    </a>
-                  </li>
-                </ul>
-                <p className="text-muted small mb-4 mb-lg-0">
-                  &copy; Your Website 2018. All Rights Reserved.
-                </p>
-              </div>
-              <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
-                <ul className="list-inline mb-0">
-                  <li className="list-inline-item mr-3">
-                    <a href="#">
-                      <i className="fab fa-facebook fa-2x fa-fw" />
-                    </a>
-                  </li>
-                  <li className="list-inline-item mr-3">
-                    <a href="#">
-                      <i className="fab fa-twitter-square fa-2x fa-fw" />
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <i className="fab fa-instagram fa-2x fa-fw" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
+
         <div>
           <Launcher
             agentProfile={{
